82ed66ea45f86d44d82ba3d8034064e23188c748
gregsadetsky <lepetitg@gmail.com>
2023-09-19 18:01:03 -0400

    .

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# i-just-want-to-see-the-plist
+# I just want to see the .plist
 
 micro project to easily convert [.plist files](https://en.wikipedia.org/wiki/Property_list) into json objects
 
 ## but why
 
 - some .plist files are xml, but kinda annoying and verbose. that's kinda fine.
-- some .plist files are binary, which is not fun because you can't see inside of them
+- some .plist files are [binary](https://medium.com/@karaiskc/understanding-apples-binary-property-list-format-281e6da00dbd), which is not fun because you can't see inside of them
 - you could [run](https://stackoverflow.com/a/10991695) `plutil -convert json Data.plist` but then that will [overwrite](https://stackoverflow.com/questions/6066350/command-line-tool-for-converting-plist-to-json#comment20774567_10991695) the original (??) so then you need to run `plutil -convert json -o Data.json Data.plist` and I'm not going to [remember](https://xkcd.com/1168/) that.
-- I think it's completely reasonable to make tools for yourself that may be useful in the future, and also for these tools to maybe useful for others. it's not the tool, it's the journey?? but it's also the tool.
-- also I would noooooot want a tool to get my .plist files on their server and convert them there..?!?! so yeah, this is all-browser-based-only. browsers are great
+- I think it's completely reasonable to make tools for yourself that may be useful to you in the future, and also for these tools to maybe be useful to others. it's not the tool, it's the journey?? but it's also the tool.
+- also I would noooooot want to use a tool that uploads my .plist files to their server and converts them there..?!?! so yeah, this is _all-browser-based-only_. browsers are honestly so great.
+- also double clicking a .plist file opens xcode, wtf seriously
 
 ## install
 
@@ -17,11 +18,11 @@ npm install # npm i
 npm run dev
 ```
 
-## build it
+## build & publish it
 
-`npm run build`
+`npm run build` , then git add, commit, push
 
-this tool is deployed to github pages
+- this tool is deployed to github pages
 
 ## huh
 
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,79 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import { BinaryPlistParserService } from "./binary-plist-parser.service";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+window.addEventListener("load", function (event) {
+  document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+    <h2 class='dropHere'>drop your .plist file here!</h2>
+  `;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  const dropZoneEl = document.querySelector(".dropHere");
+  console.log("dropZoneEl", dropZoneEl);
+
+  function dragHandlerFunction(e) {
+    console.log("dragHandlerFunction");
+
+    e.preventDefault();
+    e.stopPropagation();
+    dropZoneEl.classList.remove("fileBeingDraggedOver");
+
+    // extract base64 content from dragged file
+    const file = e.dataTransfer.files[0];
+    // read file content and convert to base64
+    const toBase64 = (file) =>
+      new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = reject;
+      });
+    toBase64(file).then((base64Content) => {
+      const base64ContentNoPrefix = base64Content.split(",")[1];
+
+      document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+      <button id='copyit' style="color:blue;font-size:25px">click here to copy the json'ified plist into your clipboard!!</button>
+    `;
+      const copyItButtonEl =
+        document.querySelector<HTMLButtonElement>("#copyit")!;
+
+      copyItButtonEl.addEventListener("click", (e) => {
+        const service = new BinaryPlistParserService();
+        navigator.clipboard.writeText(
+          JSON.stringify(service.parse64Content(base64ContentNoPrefix)),
+        );
+        copyItButtonEl.innerText = "copied! click again to copy again";
+      });
+    });
+  }
+
+  function bindTheDragHandlers(el) {
+    el.addEventListener(
+      "dragenter",
+      (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        dropZoneEl.classList.add("fileBeingDraggedOver");
+      },
+      false,
+    );
+    el.addEventListener(
+      "dragleave",
+      (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        dropZoneEl.classList.remove("fileBeingDraggedOver");
+      },
+      false,
+    );
+    el.addEventListener(
+      "dragover",
+      (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+      },
+      false,
+    );
+    el.addEventListener("drop", dragHandlerFunction, false);
+  }
+
+  bindTheDragHandlers(dropZoneEl);
+});
